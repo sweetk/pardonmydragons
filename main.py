cebbdeeb8b0b8a1cee415d543be51dca0a4f8a83
KJ Sweet <KJ Sweet>
2018-01-07 13:41:15 -0600

    A very basic blog with correctly identifying errors

--- main.py
+++ main.py
@@ -23,31 +23,42 @@ class Blog(db.Model):
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    return redirect('/blogs')
 
+@app.route('/blogs', methods=['POST', 'GET'])
+def blogs():
+    blogs = Blog.query.all()
+    return render_template('blog.html', blogs=blogs)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
     if request.method == 'POST':
+        #Error checking here?
+        title_error = ''
+        body_error = ''
+
         blog_title = request.form['blog_title']
+        if blog_title == '':
+            title_error = 'This post needs a title'
         new_blog = Blog(blog_title)
 
         blog_body = request.form['blog_body']
+        if blog_body == '':
+            body_error = 'This post needs a body'
         new_blog.post(blog_body)
 
+        #TODO edit newpost so blog title and body are preserved
+        if title_error != '' or body_error != '':
+            return render_template('newpost.html', saved_title=blog_title, saved_body=blog_body, title_error=title_error, body_error=body_error)
+
+        #If no errors
         db.session.add(new_blog)
         db.session.commit()
 
-    blogs = Blog.query.all()
-    return render_template('todos.html',title="Build-A-Blog",blogs=blogs)
+        return redirect('/blogs')
 
-
-#@app.route('/delete-task', methods=['POST'])
-#def delete_task():
-
-#    task_id = int(request.form['task-id'])
-#    task = Task.query.get(task_id)
-#    task.completed = True
-#    db.session.add(task)
-#    db.session.commit()
-
-#    return redirect('/')
+    else:
+        return render_template('newpost.html')
 
 
 if __name__ == '__main__':
