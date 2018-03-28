c40d87af70257a877707ac704f07fb3b5059a43f
KJ Sweet <ka.g.sweet@gmail.com>
2018-03-28 16:34:22 -0500

    Blank page w/database connectivity

--- main.py
+++ main.py
@@ -4,32 +4,16 @@ from hashutils import make_pw_hash, check_pw_hash
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:guest@localhost:8889/blogz'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://pardonmydragons:localhost@localhost:8889/pardonmydragons'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-app.secret_key = 'ghakrRfPLSdwOOBt5678'
-
-
-class Blog(db.Model):
-
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120))
-    body = db.Column(db.String(2000))
-    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __init__(self, title, owner):
-        self.title = title
-        self.owner = owner
-
-    def post(self, body):
-        self.body = body
+app.secret_key = '09876trcvbnmki8u7y6tgbnmkiu87ytgbn'
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(120), unique=True)
     pw_hash = db.Column(db.String(120))
-    blogs = db.relationship('Blog', backref='owner')
 
     def __init__(self, username, password):
         self.username = username
@@ -37,10 +21,9 @@ class User(db.Model):
 
 @app.before_request
 def require_login():
-    allowed_routes = ['login', 'register', 'blog', 'index']
+    allowed_routes = ['login', 'register', 'index']
     if request.endpoint not in allowed_routes and 'username' not in session:
         return redirect('/login')
-
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
@@ -56,7 +39,6 @@ def login():
             flash('User password incorrect, or user does not exist', 'error')
 
     return render_template('login.html')
-
 
 @app.route('/register', methods=['POST', 'GET'])
 def register():
@@ -82,13 +64,12 @@ def register():
             db.session.add(new_user)
             db.session.commit()
             session['username'] = username
-            return redirect('/newpost')
+            return redirect('/') #Change to welcome?
         else:
             flash("A user with that username already exists")
             return redirect('/register')
 
     return render_template('register.html')
-
 
 @app.route('/logout', methods=['POST'])
 def logout():
@@ -99,57 +80,6 @@ def logout():
 def index():
     users = User.query.all()
     return render_template('index.html', users=users)
-
-@app.route('/blog', methods=['POST', 'GET'])
-def blog():
-    blogs = Blog.query.all()
-    users = User.query.all()
-    blog_id = request.args.get('id')
-    user_id = request.args.get('user_id')
-
-    #Individual blog request
-    if blog_id:
-        blog_id = int(blog_id)
-        return render_template('individualentry.html', blogs=blogs, users=users, blog_id=blog_id)
-
-    if user_id:
-        user_id = int(user_id)
-        return render_template('individualuser.html', blogs=blogs, users=users, user_id=user_id)
-
-    #Full blog
-    return render_template('blog.html', users=users, blogs=blogs)
-
-@app.route('/newpost', methods=['POST', 'GET'])
-def newpost():
-    if request.method == 'POST':
-        title_error = ''
-        body_error = ''
-
-        owner = User.query.filter_by(username=session['username']).first()
-
-        blog_title = request.form['blog_title']
-        if blog_title == '':
-            title_error = 'This post needs a title'
-        new_blog = Blog(blog_title, owner)
-
-        blog_body = request.form['blog_body']
-        if blog_body == '':
-            body_error = 'This post needs a body'
-        new_blog.post(blog_body)
-
-        if title_error != '' or body_error != '':
-            return render_template('newpost.html', saved_title=blog_title, saved_body=blog_body, title_error=title_error, body_error=body_error)
-
-        #If no errors
-        db.session.add(new_blog)
-        db.session.commit()
-
-
-        return redirect('/blog?id=' + str(new_blog.id))
-
-    else:
-        return render_template('newpost.html')
-
 
 if __name__ == '__main__':
     app.run()
